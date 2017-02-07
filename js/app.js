8ad90658ecd996c2f88b850c77c3ea3f3921fbf1
Judd Franklin <judd@still-water.com>
2017-02-06 18:06:41 -0800

    updates to scroll behavior. Emmanuel reskin details. New background image.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+function animationSpeedByDistance (currentOffset,destinationOffset){
+  var speed = Math.abs(currentOffset-destinationOffset) < 1000 ? 2000: 3000;
+  return speed
+}
+
 $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active');
@@ -9,6 +14,17 @@ $(document).ready(function () {
     if ($(window).width() < 768){
       $('[data-toggle="offcanvas"]').click();
     }
+  });
+  $('a[href^="#"]').click(function(e){
+    var linkNumber = $(this).index('a[href^="#"]');
+    var destination = $(this)[0].hash.substring(1);
+    var destinationOffset = $(this).offset().top;
+    var currentOffset = $(window).scrollTop();
+
+    $('html,body').animate({
+      scrollTop: $("#"+destination).offset().top
+    }, animationSpeedByDistance(currentOffset,destinationOffset));
+    return false;
   });
 });
 
@@ -23,9 +39,21 @@ $(window).scroll(function () {
 var handleScroll = function(){
   var topOffset = $('#top').offset();
   if ($(window).scrollTop() > topOffset.top+300){
+    /* Toggle on "transition" class
+     * animate via css3 transitions
+     * then callback that toggles "transition" off and "sticky" on.
+     * pull-md-3 toggle needs to happen asyncronously,
+     * because it changes the layout.
+     */
     $('.sidebar-offcanvas').addClass('sticky');
     $('.main-content-column').removeClass('pull-md-3');
   } else {
+    /* Toggle on "transition" class
+     * animate via css3 transitions
+     * then callback that toggles "transition" and "sticky" off.
+     * pull-md-3 toggle needs to happen asyncronously,
+     * because it changes the layout.
+     */
     $('.sidebar-offcanvas').removeClass('sticky');
     $('.main-content-column').addClass('pull-md-3');
   };
