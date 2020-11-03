7f1bfeef2ca11a076d27f28647529b5c057ddb7f
Judd Franklin <franklin@Judds-MacBook-Pro.local>
2020-11-03 11:12:21 -0800

    minor styling and navigation touchups

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,6 +59,8 @@ class IndexPage extends React.Component {
       this.setState({
         articleTimeout: !this.state.articleTimeout
       })
+      document.body.scrollTop = 0 // For Safari
+      document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera    
     }, 350)
 
   }
