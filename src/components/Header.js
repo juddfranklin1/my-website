6cc5c6801270800c159e85eb41978aec7348476d
Judd Franklin <franklin@Judds-MacBook-Pro.local>
2020-11-06 10:07:47 -0800

    Header updates: logo as icon; functional component; some border and style updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,47 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import logo from '../images/judd-logo.png'
 
-class Header extends React.Component {
-  render() {
+const Header = (props) => (
+  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
+    <div className="logo">
+      <img src={logo} alt="Judd Franklin" />
+    </div>
+    <div className="content">
+      <div className="inner">
+        <h1>Judd Franklin: Code is for people, not computers.</h1>
+        <p>
+          Engineers should aim to satisfy people, not computers. From this belief, I have built a career producing satisfaction for site owners, end users, and my colleagues.
+        </p>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        { props.workSections.map((sect, ind) => (
+          <li key={ 'section-navigation-' + ind }>
+            <button
+              onClick={() => {
+                props.onOpenArticle(sect.node.slug)
+              }}
+            >
+              { sect.node.headline }
+            </button>  
+          </li>
+        )) }
+        <li>
+          <button
+            onClick={() => {
+              props.onOpenArticle('about')
+            }}
+          >
+            About
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+)
 
-    return (
-      <header id="header" style={this.props.timeout ? { display: 'none' } : {}}>
-        <div className="logo">
-          <span className="icon fa-code"></span>
-        </div>
-        <div className="content">
-          <div className="inner">
-            <h1>Judd Franklin: Code is for people, not computers.</h1>
-            <p>
-              Engineers should aim to satisfy people, not computers. From this belief, I have built a career producing satisfaction for site owners, end users, and my colleagues.
-            </p>
-          </div>
-        </div>
-        <nav>
-          <ul>
-            { this.props.workSections.map((sect, ind) => (
-              <li key={ 'section-navigation-' + ind }>
-                <button
-                  onClick={() => {
-                    console.log(sect.node)
-                    this.props.onOpenArticle(sect.node.slug)
-                  }}
-                >
-                  { sect.node.headline }
-                </button>  
-              </li>
-            )) }
-            <li>
-              <button
-                onClick={() => {
-                  this.props.onOpenArticle('about')
-                }}
-              >
-                About
-              </button>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    )
-  }
-}
 
 Header.propTypes = {
   onOpenArticle: PropTypes.func,
